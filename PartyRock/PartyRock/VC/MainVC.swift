95e14a1290a9dd0f83636fa30b15ada4c94aeb46
Taangh <30960806+Taangh@users.noreply.github.com>
2017-09-13 20:21:22 +0200

    Design

---
+++ PartyRock/PartyRock/VC/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by Damian on 11.09.2017.
+//  Copyright © 2017 Damian. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ PartyRock/PartyRock/VC/VideoVC.swift
@@ -0,0 +1,20 @@
+//
+//  VideoVCViewController.swift
+//  PartyRock
+//
+//  Created by Damian on 13.09.2017.
+//  Copyright © 2017 Damian. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
